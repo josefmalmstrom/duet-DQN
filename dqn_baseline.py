f90d90e025ac9b759ff846c9dc6f04e657808390
Josef Malmström <josef.malmstrom@gmail.com>
2019-05-19 22:01:03 -0700

    can now run dqn_baseline

--- dqn_baseline.py
+++ dqn_baseline.py
@@ -4,10 +4,11 @@ from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.deepq.policies import MlpPolicy
 from stable_baselines import DQN
 
-env = gym.make('CartPole-v1')
+
+env = gym.make('Duet-v0')
 env = DummyVecEnv([lambda: env])
 
-model = DQN(MlpPolicy, env, verbose=1)
+model = DQN(MlpPolicy, env, verbose=1, tensorboard_log="log/", full_tensorboard_log=True)
 model.learn(total_timesteps=25000)
 
 obs = env.reset()
