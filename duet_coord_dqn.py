a4d9ed04c9b7d5c58ce560fc23fa93a4ec07a937
Josef Malmström <josef.malmstrom@gmail.com>
2019-05-22 11:24:22 -0700

    testing smaller architecture

--- duet_coord_dqn.py
+++ duet_coord_dqn.py
@@ -48,9 +48,9 @@ def build_model(nb_actions):
     model = Sequential()
 
     model.add(Flatten(input_shape=input_shape))
-    model.add(Dense(128))
+    model.add(Dense(64))
     model.add(Activation('relu'))
-    model.add(Dense(128))
+    model.add(Dense(64))
     model.add(Activation('relu'))
     model.add(Dense(nb_actions))
     model.add(Activation('linear'))
