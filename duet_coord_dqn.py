7173188e3511a374e0d8ce255eba664baec3e1e2
Josef Malmström <josef.malmstrom@gmail.com>
2019-05-22 12:01:11 -0700

    bigger architecture in coord, removed visualization in testing for pixel

--- duet_coord_dqn.py
+++ duet_coord_dqn.py
@@ -48,9 +48,11 @@ def build_model(nb_actions):
     model = Sequential()
 
     model.add(Flatten(input_shape=input_shape))
-    model.add(Dense(64))
+    model.add(Dense(512))
     model.add(Activation('relu'))
-    model.add(Dense(64))
+    model.add(Dense(256))
+    model.add(Activation('relu'))
+    model.add(Dense(128))
     model.add(Activation('relu'))
     model.add(Dense(nb_actions))
     model.add(Activation('linear'))
